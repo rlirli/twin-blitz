c58c141729e5a1b11204432ef47f998afc2634ff
rlirli <57397330+rlirli@users.noreply.github.com>
2026-03-26 22:59:26 +0100

    feat: Configure Next.js for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Necessary because Github Pages cannot run the Next.js image optimizer
+  },
+};
 
 export default nextConfig;
